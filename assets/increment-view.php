3344467f3ac124ff4c8a17c0df0838202ae47644
Shuxrat <you@example.com>
2025-08-21 11:39:54 +0500

    partner case dtudy

--- assets/increment-view.php
+++
@@ -1,7 +0,0 @@
-<?php
-$file = 'views.txt';
-$count = file_exists($file) ? (int)file_get_contents($file) : 0;
-$count++;
-file_put_contents($file, $count);
-echo json_encode(['count' => $count]);
-?>
